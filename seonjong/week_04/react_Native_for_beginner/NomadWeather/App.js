b5480159d2c47c12b5191938a8382104b027642d
Trailblazer-Yoo <yousun9591@naver.com>
2022-07-12 17:19:04 +0900

    seonjong

--- seonjong/week_04/react_Native_for_beginner/NomadWeather/App.js
+++ seonjong/week_04/react_Native_for_beginner/NomadWeather/App.js
@@ -1,29 +1,51 @@
-import * as Location from 'expo-location'
-import React, {useEffect, useState} from "react";
-import reactDom from "react-dom";
-import { View, Text, StyleSheet, ScrollView, Dimensions } from "react-native";
+import * as Location from "expo-location";
+import React, { useEffect, useState } from "react";
+import { Fontisto } from '@expo/vector-icons'
+import {
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  Dimensions,
+  ActivityIndicator,
+} from "react-native";
 
 const API_key = '' // 어플에 두면 보안에 취약함
 
+const icons = {
+  Clouds: "cloudy",
+  Clear: "day-sunny",
+  Atmosphere: "cloudy-gusts",
+  Snow: "snow",
+  Rain: "rains",
+  Drizzle: "rain",
+  Thunderstrom : "lightning",
+}
+
 export default function App() {
-  const [city, setCity] = useState('Loading...')
-  const [days, setDays] = useState([])
-  const [ok, setOk] = useState(true)
-  const getWeather = async() => {
-    const {granted} = await Location.requestForegroundPermissionsAsync()
-    if (!granted){
-      setOk(false)
-    }
-    const {coords:{latitude, longitude}} = await Location.getCurrentPositionAsync({accuracy: 5})
-    const location = await Location.reverseGeocodeAsync({latitude, longitude}, {useGoogleMaps:false})
-    setCity(location[0].city)
-    const response = await fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&exclude=alerts&appid=${API_key}`)
-    const json = await response.json()
-    console.log(json)
-  }
+  const [city, setCity] = useState("loading...");
+  const [days, setDays] = useState([]);
+  const [ok, setOk] = useState(true);
+  const getPermission = async () => {
+    const { granted } = await Location.getForegroundPermissionsAsync();
+    if (!granted) setOk(false);
+    const {
+      coords: { latitude, longitude },
+    } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+    const location = await Location.reverseGeocodeAsync(
+      { latitude, longitude },
+      { useGoogleMaps: false }
+    );
+    setCity(location[0].city);
+    const weatherAPI = await fetch(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=alerts&appid=${API_key}`
+    );
+    const weatherJson = await weatherAPI.json();
+    setDays(weatherJson.daily);
+  };
   useEffect(() => {
-    getWeather()
-  },[])
+    getPermission();
+  }, []);
   return (
     <View style={styles.container}>
       <View style={styles.city}>
@@ -35,33 +57,32 @@ export default function App() {
         horizontal
         contentContainerStyle={styles.weather}
       >
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.decription}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.decription}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.decription}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.decription}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.decription}>Sunny</Text>
-        </View>
+        {days.length === 0 ? (
+          <View style={{...styles.day, alignItems:"center"}}>
+            <ActivityIndicator
+              color="white"
+              style={{ marginTop: 10 }}
+              size="large"
+            />
+          </View>
+        ) : (
+          days.map((day, index) =>
+          <View key={index} style={styles.day}>
+            <View style={{ flexDirection: "row", alignItems:"center", width:'100%', justifyContent:'space-between' }}>
+              <Text style={styles.temp}>{parseFloat(day.temp.day).toFixed(1)}</Text>
+              <Fontisto name={icons[day.weather[0].main]} size={68} color="white" />
+            </View>
+            <Text style={styles.decription}>{day.weather[0].main}</Text>
+            <Text style={styles.tinyText}>{day.weather[0].description}</Text>
+          </View>
+            ) 
+        )}
       </ScrollView>
     </View>
   );
 }
 
 const { width: ScreenWidth } = Dimensions.get("window");
-
 
 const styles = StyleSheet.create({
   container: {
@@ -84,12 +105,15 @@ const styles = StyleSheet.create({
   },
   temp: {
     marginTop: 50,
-    fontSize: 178,
+    fontSize: 100,
   },
   decription: {
     marginTop: -30,
     fontSize: 60,
   },
+  tinyText: {
+    fontSize:20
+  }
 });
 
 // 1. View는 container이다. div 대신에 View를 사용한다.
