6a0754686a833f7c47c1caf7cd22220eb3134b5a
jjh0987 <djflswlstlf@naver.com>
2022-07-31 16:09:10 +0900

    2022-07-31_jh

---
+++ Junho/react/react-for-jh/src/App.js
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+import Button from "./Button";
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome back!</h1>
+      <Button text={'hello i am button.'}/>
+      <Button/>
+    </div>
+  );
+}
+
+export default App;
