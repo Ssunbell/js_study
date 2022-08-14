7e6144248642696834eea05e7f995042d8b3afca
Trailblazer-Yoo <yousun9591@naver.com>
2022-08-15 01:13:51 +0900

    seonjong

--- seonjong/week_09/IG_Clone_Coding/firebase.js
+++ seonjong/week_09/IG_Clone_Coding/firebase.js
@@ -1,4 +1,7 @@
-import firebase from 'firebase'
+import firebase from "firebase/compat/app";
+import 'firebase/compat/auth'
+import 'firebase/compat/firestore'
+import 'firebase/compat/storage'
 
 const firebaseConfig = {
   apiKey: "AIzaSyByUy8ccHkkFTg3xVOEeA_hFh1zyEW68c4",
@@ -7,8 +10,8 @@ const firebaseConfig = {
   storageBucket: "instagram-clone-coding-4a8fc.appspot.com",
   messagingSenderId: "238111638145",
   appId: "1:238111638145:web:e0080674c37cbb0cfd8380"
-};
+  };
 
-!firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app
+!firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app()
 
-export default firebase
+export default firebase;
